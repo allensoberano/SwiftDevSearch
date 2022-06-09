9695abefddcf31d12118ffe5e333dfeb1319a509
Allen <allensoberano@gmail.com>
2022-06-09 11:39:59 -0700

    initial commit

---
+++ SwiftJobSearch/SwiftJobSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftJobSearch
+//
+//  Created by Allen Soberano on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
