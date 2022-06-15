08787314e61c668139dda4c8cfb376153a6cebee
Allen <allensoberano@gmail.com>
2022-06-15 09:02:33 -0700

    Table Header and Spinner Activity

--- SwiftJobSearch/SwiftJobSearch/UI/JobSearchViewController.swift
+++ SwiftJobSearch/SwiftJobSearch/UI/JobSearchViewController.swift
@@ -11,9 +11,15 @@ import SnapKit
 
 class JobSearchViewController: UIViewController {
 
+    struct JobConstants {
+        static let navTitle = "Job Search"
+        static let tableViewHeaderImage = "jobSearchHeader"
+    }
+
     let viewModel: JobSearchViewModel
     let provider = MoyaProvider<MuseJobTarget>()
     let tableView = UITableView()
+    let spinner = UIActivityIndicatorView(style: .large)
 
     init() {
         viewModel = JobSearchViewModel()
@@ -39,11 +45,12 @@ class JobSearchViewController: UIViewController {
 extension JobSearchViewController: ViewContainer {
     func styleView() {
         view.backgroundColor = .white
-        navigationItem.title = "Job Search"
+        navigationItem.title = JobConstants.navTitle
     }
 
     func addSubviews() {
         addTableView()
+        addSpinnerView()
     }
 
     private func addTableView() {
@@ -54,6 +61,19 @@ extension JobSearchViewController: ViewContainer {
         tableView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
+    }
+
+    private func addSpinnerView() {
+        view.addSubview(spinner)
+        spinner.hidesWhenStopped = true
+        spinner.color = .orange
+        spinner.sizeToFit()
+
+        spinner.snp.makeConstraints { (make) in
+            make.center.equalTo(tableView.snp.center)
+        }
+
+        spinner.startAnimating()
     }
 }
 
@@ -73,11 +93,23 @@ extension JobSearchViewController: UITableViewDelegate, UITableViewDataSource {
         let viewController = JobDetailsViewController(job: viewModel.jobs[indexPath.row])
         navigationController?.pushViewController(viewController, animated: true)
     }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: JobConstants.tableViewHeaderImage)
+        return imageView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 85
+    }
 }
 
 extension JobSearchViewController: JobSearchViewModelProtocol {
     func onUpdateJobs() {
         print(viewModel.jobs)
+        spinner.stopAnimating()
         tableView.reloadData()
     }
 }
